c3c2e1c5cb87d0984156f23ece3a188906c7b407
Benjamin Pablo <benjaminpablo2345@gmail.com>
2022-06-06 16:07:00 -0500

    Working with the slider component

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,7 @@
 'use strict';
 
-const btnMobile = document.querySelector('.btn__mobile');
-const btnMobileBackground = document.querySelector('.btn__background');
-const navEl = document.querySelector('.nav');
-const navListEl = document.querySelector('.nav__list');
 const navLinkEl = document.querySelectorAll('.nav__link');
 
-btnMobile.addEventListener('click', function () {
-  btnMobileBackground.classList.toggle('btn__background--open');
-  navEl.classList.toggle('nav--mobile');
-  navListEl.classList.toggle('nav__list--mobile');
-  navLinkEl.forEach(el => {
-    el.classList.toggle('nav__link--mobile');
-  });
+navLinkEl.forEach(link => {
+  link.addEventListener('click', () => {});
 });
